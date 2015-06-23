c33ae0b20f49335c055810079c5fed1bfd41f9cd
GIT ddeveloperr <ddeveloperr@gmail.com>
2015-06-24 01:48:30 +0200

    Build note app using React and Flux

---
+++ stores/NoteStore.js
@@ -0,0 +1,54 @@
+/* As you see we listen to two actions, createNote and editNote, inside the init method. 
+   We also register callbacks to execute when actions are invoked. 
+   The code for adding/updating a note is pretty straightforward. 
+   We also expose getters to retrieve list of notes. 
+   Finally, the store is exported so that it can be used in our component.*/
+
+var Reflux=require('reflux');
+var NoteActions=require('../actions/NoteActions');
+
+var _notes=[]; //This is private notes array
+
+var NoteStore = Reflux.createStore({
+
+    init: function() {
+         // Here we listen to actions and register callbacks
+        this.listenTo(NoteActions.createNote, this.onCreate);
+        this.listenTo(NoteActions.editNote, this.onEdit);
+    },
+
+    onCreate: function(note) {
+        _notes.push(note); //create a new note
+        
+        // Trigger an event once done so that our components can update. Also pass the modified list of notes.
+        this.trigger(_notes);
+    },
+
+    onEdit: function(note) {
+         // Update the particular note item with new text.
+        for(var i=0;i<_notes.length;i++){
+            if(_notes[i]._id===note._id){
+                _notes[i].text=note.text;
+                this.trigger(_notes);
+                break;
+            }
+        }
+    },
+     
+    //getter for notes 
+    getNotes:function(){
+        return _notes;
+    },
+    
+    //getter for finding a single note by id
+    getNote:function(id){
+        for(var i=0;i<_notes.length;i++){
+            if(_notes[i]._id===id){
+                return _notes[i];
+            }
+        }
+    }
+
+});
+
+module.exports=NoteStore;   //Finally, export the Store
